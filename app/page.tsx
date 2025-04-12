2e3e9d58f838f154343dc7c464c537141e2058a2
Jon-James Davies <j.jdavies@hotmail.co.uk>
2025-04-12 17:31:08 +0100

    Initial design and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,73 @@
+"use client";
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./output.module.css";
+
+import MainLogo from "./img/branding/main-logo.png";
+import { useState } from "react";
 
 export default function Home() {
+  const bannerCategories = [
+    { name: "Cordless Tools" },
+    { name: "Corded Tools" },
+    { name: "Garden Tools" },
+    { name: "Generators" },
+    { name: "Accessories" },
+  ];
+
+  const [hoverCategory, setHoverCategory] = useState<string>(""); // State to track which category is being hovered over
+
+  const mouseEnter = (e) => {
+    setHoverCategory(e.target.innerText);
+  };
+
+  const mouseLeave = (e) => {
+    console.log(e);
+    // if (hoverCategory === e.target.innerText) {
+    setHoverCategory("");
+    // }
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div className={styles.salesBanner}>!!SALE!!</div>
+        <div className={styles.mainBanner}>
+          <div className={styles.mainLogoContainer}>
             <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className={styles.mainLogo}
+              src={MainLogo}
+              width={0}
+              height={0}
+              alt="company logo"
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className={styles.bannerCategories}>
+            {bannerCategories.map((category) => (
+              <div
+                key={category.name}
+                className={styles.category}
+                onMouseEnter={mouseEnter}
+                onMouseLeave={mouseLeave}
+              >
+                {category.name}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={styles.bannerDropDownMenus}>
+          {bannerCategories.map((category, index) => (
+            <div
+              key={category.name}
+              className={styles.bannerDropDown}
+              style={{
+                display: hoverCategory === category.name ? "block" : "none",
+                left: index * (40 / bannerCategories.length) + 10 + "%",
+              }}
+            ></div>
+          ))}
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
